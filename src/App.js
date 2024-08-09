392b28933156841fb506fe25cdfccf72dc2f76d3
rjgeng <rj.geng@gmail.com>
2024-08-09 13:39:54 -0700

    added theme-overrides

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "../styles/App.less";
 
 export function App() {
     return (
-        <div>
+        <div className="landing-main-text">
             <h1>Welcome to Labs Basic SPA</h1>
-            <div>
+            <div className="landing-main-text">
                 <p>
                 This is an example of how we&apos;d like for you to use <span>antd</span>.
                 </p>
